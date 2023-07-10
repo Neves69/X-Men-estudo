0013f203938c3ff09863f6e15061903d271b2458
Neves69 <132582398+Neves69@users.noreply.github.com>
2023-07-09 23:24:06 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+const personagens = document.querySelectorAll('.personagem');
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener('mouseenter', () => {
+        if(window.innerWidth < 450 ){
+            window.scrollTo({top: 0, behavior: 'smooth'});
+        }
+        removerSelecaoPersonagem();
+
+        personagem.classList.add('selecionado');
+
+        alterarImagemPersonagem(personagem);
+
+        alterarNomePersonagem(personagem);
+
+        alterarDescricaoPersonagem(personagem);
+    })
+})
+
+function alterarDescricaoPersonagem(personagem) {
+    const descPersonagem = document.getElementById('descricao-personagem');
+    descPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagem(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagem(personagem) {
+    const imgPersonagemGrande = document.querySelector('.personagem-grande');
+    const idPersonagem = personagem.attributes.id.value;
+    imgPersonagemGrande.src = `/src/img/card-${idPersonagem}.png`;
+}
+
+function removerSelecaoPersonagem() {
+    const personagemSelecionado = document.querySelector('.selecionado');
+    personagemSelecionado.classList.remove('selecionado');
+}
