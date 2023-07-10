a2ecca63c8dd49fbf307ac199a0724d11af0d3c1
Neves69 <132582398+Neves69@users.noreply.github.com>
2023-07-09 23:33:25 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -30,7 +30,7 @@ function alterarNomePersonagem(personagem) {
 function alterarImagemPersonagem(personagem) {
     const imgPersonagemGrande = document.querySelector('.personagem-grande');
     const idPersonagem = personagem.attributes.id.value;
-    imgPersonagemGrande.src = `/src/img/card-${idPersonagem}.png`;
+    imgPersonagemGrande.src = `./src/img/card-${idPersonagem}.png`;
 }
 
 function removerSelecaoPersonagem() {
